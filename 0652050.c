c84b31b6f9b990cd2bdb86a25d2965480ced2021
ekkoa <39372982+ekkoa@users.noreply.github.com>
2018-05-17 23:51:48 +0800

    0652050

---
+++ 0652050.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int a,b,sum;
+
+	
+	printf("number1\n");
+	scanf("%d",&a);
+	
+	printf("number2\n");
+	scanf("%d",&b);
+	
+	sum=a+b;
+	
+	printf("sum=%d\n",sum);
+	
+	return 0;
+}
